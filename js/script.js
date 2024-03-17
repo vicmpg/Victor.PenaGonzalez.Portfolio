e6a002328e35b0d9b148d9949bd67bab09842fca
vicmpg <vicmpg22@gmail.com>
2024-03-17 14:30:55 -0400

    Changing styling

--- js/script.js
+++ js/script.js
@@ -1,45 +1,16 @@
-(function() {
-    let form = document.querySelector('#contact-form');
-    let emailInput = document.querySelector('#contact-email');
-    // function to show error message
-    function showErrorMessage(input, message) {
-        let container = input.previousElementSibling;
-        //Check and remove any existing errors
-        let error = container.querySelector('.error-message');
-        if (error) {
-            container.removeChild(error);
-        }
-        // Add error if the message isn't empty
-        if (message) {
-            let error = document.createElement('div');
-            error.classList.add('error-message');
-            error.innerText = message;
-            container.appendChild(error);
-        }
-    }
-    // function to validate email
-    function validateEmail() {
-        let value = emailInput.value;
-        if(!value) {
-            showErrorMessage(emailInput, 'Email is a required field.');
-            return false;
-        }
-        if (value.indexOf('@') === -1) {
-            showErrorMessage(emailInput, 'You must enter a valid email address.');
-            return false;
-        }
-        if (value.indexOf('.') === -1) {
-            showErrorMessage(emailInput, 'You must enter a valid email address.');
-            return false;
-        }
-        showErrorMessage(emailInput, null);
-        return true;
-    }
-    // function validate form
-    /*function validateForm() {
-        let isValidEmail = validateEmail();
-        return isValidEmail;
-    }*/
-
-    emailInput.addEventListener('input', validateEmail);
-})();
+let sections = document.querySelectorAll('section');
+let menuItems = document.querySelectorAll('.navigation-list__item');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            menuItems.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.navigation-list__item[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
